4c003f4f995f3782620335a852da767528a54935
Loruski <lorusso116@gmail.com>
2023-11-17 15:23:50 +0100

    aggiunta generazione random di ostacoli

--- Prima consegna/Codice/agentTop.py
+++ Prima consegna/Codice/agentTop.py
@@ -1,6 +1,11 @@
 # agentTop.py - Top Layer
 # AIFCA Python3 code Version 0.9.3 Documentation at http://aipython.org
 # Download the zip file and read aipython.pdf for documentation
+import random
+
+from agentMiddle import Rob_middle_layer
+from agents import Environment
+
 
 # Artificial Intelligence: Foundations of Computational Agents http://artint.info
 # Copyright David L Poole and Alan K Mackworth 2017-2021.
@@ -8,13 +13,10 @@
 # Attribution-NonCommercial-ShareAlike 4.0 International License.
 # See: http://creativecommons.org/licenses/by-nc-sa/4.0/deed.en
 
-from agentMiddle import Rob_middle_layer
-from agents import Environment
-
 
 class Rob_top_layer(Environment):
-    def __init__(self, middle, timeout=200, locations = {'mail':(-5,10), 
-                          'o103':(50,10), 'o109':(100,10),'storage':(101,51)} ):
+    def __init__(self, middle, timeout=200, locations={'mail': (-5, 10),
+                                                       'o103': (50, 10), 'o109': (100, 10), 'storage': (101, 51)}):
         """middle is the middle layer
         timeout is the number of steps the middle layer goes before giving up
         locations is a loc:pos dictionary 
@@ -23,7 +25,7 @@ class Rob_top_layer(Environment):
         self.middle = middle
         self.timeout = timeout  # number of steps before the middle layer should give up
         self.locations = locations
-        
+
     def do(self, plan):
         """carry out actions.
         actions is of the form {'visit':list_of_locations}
@@ -49,7 +51,6 @@ class Rob_top_layer(Environment):
             to_do.remove(next)
             print("******** RIMANENTI:", to_do)
 
-
     def min_distance(self, location_list):  # calcola la distanza tra la posizione attuale e la posizione successiva
         """return the nearest location in location_list
         """
@@ -68,9 +69,11 @@ class Rob_top_layer(Environment):
         """
         target_x, target_y = target
         agent_x, agent_y = self.middle.percepts['rob_x_pos'], self.middle.percepts['rob_y_pos']
-        return (target_x-agent_x)**2 + (target_y-agent_y)**2
+        return (target_x - agent_x) ** 2 + (target_y - agent_y) ** 2
+
 
 import matplotlib.pyplot as plt
+
 
 class Plot_env(object):
     def __init__(self, body, top):
@@ -80,44 +83,56 @@ class Plot_env(object):
         plt.ion()
         plt.clf()
         plt.axes().set_aspect('equal')
-        for wall in body.env.walls:     #create the walls
-            ((x0,y0),(x1,y1)) = wall
-            plt.plot([x0,x1],[y0,y1],"-k",linewidth=3)
-        for loc in top.locations:    # create the locations
-            (x,y) = top.locations[loc]
-            plt.plot([x],[y],"k<")
-            plt.text(x+1.0,y+0.5,loc) # print the label above and to the right
-        plt.plot([body.rob_x],[body.rob_y],"go")
+        for wall in body.env.walls:  # create the walls
+            ((x0, y0), (x1, y1)) = wall
+            plt.plot([x0, x1], [y0, y1], "-k", linewidth=3)
+        for loc in top.locations:  # create the locations
+            (x, y) = top.locations[loc]
+            plt.plot([x], [y], "k<")
+            plt.text(x + 1.0, y + 0.5, loc)  # print the label above and to the right
+        plt.plot([body.rob_x], [body.rob_y], "go")
         plt.draw()
 
     def plot_run(self):
         """plots the history after the agent has finished.
         This is typically only used if body.plotting==False
         """
-        xs,ys = zip(*self.body.history)
-        plt.plot(xs,ys,"go")
-        wxs,wys = zip(*self.body.wall_history)
-        plt.plot(wxs,wys,"ro")
+        xs, ys = zip(*self.body.history)
+        plt.plot(xs, ys, "go")
+        wxs, wys = zip(*self.body.wall_history)
+        plt.plot(wxs, wys, "ro")
         plt.draw()
 
 
 from agentEnv import Rob_body, Rob_env
 
-env = Rob_env({((20,0),(30,20)), ((70,-5),(70,25))})
+OBSTACLES = 4
+
+
+def make_obstacles(n):
+    """make n obstacles
+    """
+    obstacles = set()
+    for i in range(n):
+        x = random.randint(0, 100)
+        y = random.randint(0, 100)
+        obstacles.add((x, y))
+    return obstacles
+
+
+# env = Rob_env({((20,0),(30,20)), ((70,-5),(70,25))})
+env = Rob_env(make_obstacles(OBSTACLES))
 body = Rob_body(env)
 middle = Rob_middle_layer(body)
 top = Rob_top_layer(middle)
 
 # try:
-pl=Plot_env(body,top)
-top.do({'visit':['o109', 'storage', 'o103']})
+pl = Plot_env(body, top)
+top.do({'visit': ['o109', 'storage', 'o103']})
 pl.plot_run()
 # You can directly control the middle layer:
-middle.do({'go_to':(30,-10), 'timeout':200})
-
+middle.do({'go_to': (30, -10), 'timeout': 200})
 
 # do the following to see the history of the robot:
 # pl.plot_run()   #for the robot trap
 plt.show()
-
-
