0edf01ae76ffd032d426e95a187d1168996e25c4
Giacomix02 <paoloccigiacomo@gmail.com>
2023-11-23 23:46:21 +0100

    si consegna!

--- Terza consegna/Codice/main.py
+++ Terza consegna/Codice/main.py
@@ -7,6 +7,8 @@ import copy
 
 domain = [1,2,3,4]
 variables = [Variable("A", domain, position=(0.35, 0.03)), Variable("B", domain, position=(0.02,0.3)), Variable("C", domain, position=(0.5, 0.93)), Variable("D", domain, position=(0.65,0.09)), Variable("E", domain, position=(1, 0.1))]
+nuoveVariabili = [Variable("A", domain, position=(0.35, 0.03)), Variable("B", domain, position=(0.02,0.3)), Variable("C", domain, position=(0.5, 0.93)), Variable("D", domain, position=(0.65,0.09)), Variable("E", domain, position=(1, 0.1))]
+constraint = []
 constraint = []
 constraint.append(Constraint([variables[0], variables[3]], lambda x, y: x > y, "A>D"))
 constraint.append(Constraint([variables[3], variables[4]], lambda x, y: x > y, "D>E"))
@@ -17,7 +19,7 @@ constraint.append(Constraint([variables[1], variables[0]], lambda x, y: x >= y, 
 constraint.append(Constraint([variables[1], variables[2]], lambda x, y: x != y, "B!=C"))
 constraint.append(Constraint([variables[2], variables[3]], lambda x, y: x != y + 1, "C!=D+1"))
 
-USE_DFS = False
+USE_DFS = True
 USE_ARC_CONSISTENCY = True
 USE_AC_WITH_DOMAIN_SPLITTING = True
 
